bc7be0dc6186de570f7e1a3978c979af923de552
Mediatekuser <estrogajohnrhys@gmail.com>
2026-05-29 12:33:03 +0000

    ini

---
+++ omni_TECNO-BG7.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from TECNO-BG7 device
+$(call inherit-product, device/tecno/TECNO-BG7/device.mk)
+
+PRODUCT_DEVICE := TECNO-BG7
+PRODUCT_NAME := omni_TECNO-BG7
+PRODUCT_BRAND := TECNO
+PRODUCT_MODEL := TECNO BG7
+PRODUCT_MANUFACTURER := tecno
+
+PRODUCT_GMS_CLIENTID_BASE := android-tecno
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vnd_bg7_xe674s-user 12 SP1A.210812.016 980774 release-keys"
+
+BUILD_FINGERPRINT := TECNO/BG7-OPPJ/TECNO-BG7:12/SP1A.210812.016/260121V152:user/release-keys
